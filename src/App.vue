ce1d0f815ace68ab64abf87d0dac51c1bc8a9569
Blaquewithaq <blaquewithaq@gmail.com>
2023-01-08 10:19:28 -0600

    Init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import ProjectM from './components/ProjectM.vue'
+</script>
+
+<template>
+  <div class="container">
+    <h1>AnomieVision | Plugin | ProjectM</h1>
+
+    <ProjectM />
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/ProjectM.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import avPluginProjectM from "av-plugin-projectm";
+
+const canvasEl = ref();
+let avPlugin: avPluginProjectM;
+
+onMounted(async () => {
+  const canvas = {
+    canvas: canvasEl.value,
+    wasmFilepath: "assets/wasm/av_plugin_projectm_wasm_rs.wasm",
+  };
+
+  avPlugin = new avPluginProjectM(canvas);
+
+  await avPlugin.initialize();
+  console.log(await avPlugin);
+}); 
+</script>
+
+<template>
+  <div class="projectm">
+    <canvas
+      id="canvas"
+      ref="canvasEl"
+      oncontextmenu="event.preventDefault()"
+      tabindex="-1"
+    ></canvas>
+  </div>
+</template>
